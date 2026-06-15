39fec46bc8111808e29169d1c64730ed6b2260d8
pacmandoh <pacmandoh@outlook.com>
2026-06-15 19:59:04 +0800

    refactor(desktop): move MSFX dialog models to `DialogService`

--- apps/desktop-avalonia/src/ViewModels/Pages/MsfxLinkViewModel.cs
+++ apps/desktop-avalonia/src/ViewModels/Pages/MsfxLinkViewModel.cs
@@ -17,7 +17,6 @@ using PacToolkits.Application.Abstractions;
 using PacToolkits.Application.Services;
 using PacToolkits.Desktop.Avalonia.Services.Infrastructure;
 using PacToolkits.Desktop.Avalonia.Services.Integration;
-using PacToolkits.Desktop.Avalonia.Views.Dialogs;
 
 namespace PacToolkits.Desktop.Avalonia.ViewModels.Pages;
 
